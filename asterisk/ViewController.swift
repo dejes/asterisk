d20fe8d2535602b4b02b7956e5b1160f044ee75a
hsumax0216 <b26262235@gmail.com>
2019-05-10 16:36:18 +0800

    Initial Commit

---
+++ asterisk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asterisk
+//
+//  Created by t450sMAC on 2019/5/10.
+//  Copyright © 2019 t450sMAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
